88587e4d516d9f07a26d128134c054d0ecf96b9b
admin <admin@DESKTOP-H6SUMKT>
2020-05-29 15:30:18 +0900

    갱신 확인

--- Sample/src/ch04/Baseball.java
+++ Sample/src/ch04/Baseball.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Scanner;
-
+// 
 public class Baseball {
 	public static void main(String[] args) {
 		Number n = new Number();
--- Sample/src/주사위게임.java
+++ Sample/src/주사위게임.java
@@ -6,7 +6,6 @@ public class 주사위게임 {
 		while (isContinue) {
 			int num1 = (int) (Math.random() * 6 + 1);
 			int num2 = (int) (Math.random() * 6 + 1);
-
 			count++; // 던진 횟수 증가 한번 돌때마다 1씩 증가.
 
 			System.out.printf("(%s,%s)\n", num1, num2);

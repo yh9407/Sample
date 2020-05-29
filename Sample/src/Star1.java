a59ff10046c14157e108654da6ada4a80d87e41c
admin <admin@DESKTOP-H6SUMKT>
2020-05-29 17:49:05 +0900

    05-29

--- Sample/src/Star1.java
+++ Sample/src/Star1.java
@@ -2,6 +2,7 @@ public class Star1 {
 	public static void main(String[] args) {
 		for (int a = 1; a <= 5; a++) {
 			for (int b = 1; b <= a; b++) {
+				
 				System.out.print("*");
 			}
 			System.out.println();
---
+++ Sample/src/ch04/StarArt.java
@@ -0,0 +1,27 @@
+package ch04;
+
+public class StarArt {
+	public static void main(String[] args) {
+		int center = 9 / 2 + 1;
+
+		for (int i = 0; i <= center; i++) {// yÃà
+
+			for (int j = 0; j <= center - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 2 * i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = 4; i >= 0; i--) {
+			for (int j = 0; j <= center - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 2 * i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}

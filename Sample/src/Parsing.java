0d60c25d227aaf227b4e9750ac0ae3d57e623392
admin <admin@DESKTOP-H6SUMKT>
2020-06-08 17:48:37 +0900

    06-08

--- Sample/src/Parsing.java
+++ Sample/src/Parsing.java
@@ -1,30 +1,30 @@
-import java.io.IOException;
-import java.util.Scanner;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-public class Parsing {
-	public static void main(String[] args) throws IOException {
-		Document doc = 
-				Jsoup.connect("https://comic.naver.com/webtoon/list.nhn?titleId=651673&weekday=sat").get(); 
-		String title = doc.title();
-		System.out.println(title);
-		
-		Elements es = doc.select("td.title > a");
-		for(int i = 0; i < es.size(); i++) {
-			Element e = es.get(i);
-			String s = e.text();
-			System.out.println(s);
-			
-			String href = e.attr("href");
-			System.out.println(href);
-		}
-	}
-}
-
-
-
-
+//import java.io.IOException;
+//import java.util.Scanner;
+//
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+//import org.jsoup.select.Elements;
+//
+//public class Parsing {
+//	public static void main(String[] args) throws IOException {
+//		Document doc = 
+//				Jsoup.connect("https://comic.naver.com/webtoon/list.nhn?titleId=651673&weekday=sat").get(); 
+//		String title = doc.title();
+//		System.out.println(title);
+//		
+//		Elements es = doc.select("td.title > a");
+//		for(int i = 0; i < es.size(); i++) {
+//			Element e = es.get(i);
+//			String s = e.text();
+//			System.out.println(s);
+//			
+//			String href = e.attr("href");
+//			System.out.println(href);
+//		}
+//	}
+//}
+//
+//
+//
+//
--- Sample/src/Parsing2.java
+++ Sample/src/Parsing2.java
@@ -1,30 +1,30 @@
-import java.io.IOException;
-import java.util.Scanner;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-public class Parsing2 {
-	public static void main(String[] args) throws IOException {
-		Document doc = 
-				Jsoup.connect("https://comic.naver.com/webtoon/list.nhn?titleId=651673&weekday=sat").get(); 
-		String title = doc.title();
-		System.out.println(title);
-		
-		파일다운로드 f = new 파일다운로드();
-		
-		Elements es = doc.select("table.viewList tr img");
-		for(int i = 0; i < es.size(); i++) {
-			Element e = es.get(i);
-			String href = e.attr("src");
-			f.saveImage(href, i + ".jpg");
-			System.out.println(href);
-		}
-	}
-}
-
-
-
-
+//import java.io.IOException;
+//import java.util.Scanner;
+//
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+//import org.jsoup.select.Elements;
+//
+//public class Parsing2 {
+//	public static void main(String[] args) throws IOException {
+//		Document doc = 
+//				Jsoup.connect("https://comic.naver.com/webtoon/list.nhn?titleId=651673&weekday=sat").get(); 
+//		String title = doc.title();
+//		System.out.println(title);
+//		
+//		파일다운로드 f = new 파일다운로드();
+//		
+//		Elements es = doc.select("table.viewList tr img");
+//		for(int i = 0; i < es.size(); i++) {
+//			Element e = es.get(i);
+//			String href = e.attr("src");
+//			f.saveImage(href, i + ".jpg");
+//			System.out.println(href);
+//		}
+//	}
+//}
+//
+//
+//
+//
